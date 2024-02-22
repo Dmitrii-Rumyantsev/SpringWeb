9a6ee1058efb91049a1f417fd99d4479f7bf53f7
Dmitrii <dima33773@gmail.com>
2024-02-22 18:33:45 +0300

    Initial commit

---
+++ src/main/java/com/example/firstprojectspringweb/FirstProjectSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstprojectspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectSpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstprojectspringweb/FirstProjectSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstprojectspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
